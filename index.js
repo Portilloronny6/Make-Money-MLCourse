c22490bb916c30cc8c189dce12a3615ab15578e1
Ronny Portillo <41457625+Portilloronny6@users.noreply.github.com>
2019-08-29 17:16:06 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -53,15 +53,13 @@ async function setupWebcam() {
   return new Promise((resolve, reject) => {
     const navigatorAny = navigator;
     navigator.getUserMedia = navigator.getUserMedia ||
-      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-      navigatorAny.msGetUserMedia;
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
     if (navigator.getUserMedia) {
-      navigator.getUserMedia({
-          video: true
-        },
+      navigator.getUserMedia({video: true},
         stream => {
           webcamElement.srcObject = stream;
-          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
         },
         error => reject());
     } else {
